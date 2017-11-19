218cea69a9e668ad62c5c497c18a97947ba964d0
ReffoGionni <gionni.reffo@studenti.unitn.it>
2017-11-19 21:26:54 +0000

    Aggiunti test per customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,33 @@ class Customer < ApplicationRecord
   has_many :invoice, dependent: :destroy
   has_many :issue
   has_many :user, through: :issue
+  
+
+  #cf, name, address, postcode, city, prov
+  def hasMinData?
+    !(cf.nil?) && !(name.nil?) && !(address.nil?) && !(postcode.nil?) && !(city.nil?) && !(prov.nil?)
+  end
+  
+  # Semplice verifica di validità del codice fiscale
+  # Accettato solo numerico lungo 11 o alfanumerico lungo 16
+  def hasValidCf?
+    return true if (cf.length == 11) && (cf[/[0-9]+/] == cf)
+    return true if cf =~ /[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]/
+    false
+  end
+
+  def valid? (context=nil)
+      context ||= (new_record? ? :create : :update)
+      output = super(context)
+
+      errors.add(:cf, "must be valid") unless hasValidCf?
+      errors.add(:name, "must have two or more chars") unless name.length >= 2
+      errors.add(:address, "must have two or more chars") unless address.length >= 2
+      errors.add(:postcode, "must exist") if postcode.nil?
+      errors.add(:city, "must have two or more chars") unless city.length >= 2
+      errors.add(:prov, "must have two chars") unless prov.length == 2
+      
+      errors.empty? && output
+  end
+  
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,33 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  describe "Customer - scenario: Inserting a new customer that" do
+
+    it "has a cf, name, address, postcode, city, prov" do
+      @customer = Customer.new(:cf => "aaa", :name => "Gionni", :surname => "Reffo", :address => "Via Sommarive, 5", :postcode => 38123, :city => "Povo", :prov => "TN")
+      expect(@customer.hasMinData?).to be true
+    end
+
+    it "has a valid alphanumeric cf" do
+      @customer = Customer.new(:cf => "ABCDEF12G34H567I")
+      expect(@customer.hasValidCf?).to be true
+    end
+
+    it "has a invalid cf 16" do
+      @customer = Customer.new(:cf => "ABCDEF12G34H5678")
+      expect(@customer.hasValidCf?).to be false
+    end
+
+    it "has a valid numeric cf" do
+      @customer = Customer.new(:cf => "12345678901")
+      expect(@customer.hasValidCf?).to be true
+    end
+
+    it "has a invalid cf 11" do
+      @customer = Customer.new(:cf => "1234567890A")
+      expect(@customer.hasValidCf?).to be false
+    end
+  end
+
 end
